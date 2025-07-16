0a31dc98d76d2adea2eafe662bc8a22933ac0d48
gptkong <gptkong>
2025-07-16 14:51:48 +0800

    change card grid col

--- components/NavigationDashboard.tsx
+++ components/NavigationDashboard.tsx
@@ -124,7 +124,7 @@ export function NavigationDashboard() {
             ))}
           </div>
         ) : filteredLinks.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
+          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
             {filteredLinks.map((link) => (
               <NavigationCard key={link.id} link={link} />
             ))}
